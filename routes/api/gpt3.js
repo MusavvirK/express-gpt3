2f391ff19356715a7d5a3be4888d6b9f70ced7b7
musavvir <abde.khan@newscred.com>
2021-06-15 21:40:46 +0600

    gpt3 call fix

--- routes/api/gpt3.js
+++ routes/api/gpt3.js
@@ -3,33 +3,39 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const API_KEY = '';
-const URL = 'https://api.openai.com/v1/engines/davinci/completions';
+const url = 'https://api.openai.com/v1/engines/davinci/completions';
 
 router.get('/', (req, res) => res.send('<h1>Successful</h1>'));
 
 
 // Get Single Member
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   console.log('hit successful');
+  const method = 'post';
+  const prompt = req.body.querystring;
 
-  const generatedcontent = await axios({
-    URL,
-    headers: {
-      'Authorization': `Bearer ${API_KEY}`,
-      'Content-Type': 'application/json'
-    },
-    data: {
-      "prompt": "Once upon a time",
-      "max_tokens": 5
-    },
-    'post'
-  });
+  (async () => {
+    const generatedcontent = await axios({
+      url,
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        "prompt": prompt,
+        "max_tokens": 100
+      },
+      method,
+    });
 
-  if (generatedcontent) {
-    res.json(generatedcontent);
-  } else {
-    res.status(400).json({ msg: `Content generation failed` });
-  }
+    if (generatedcontent.data){
+      console.log(generatedcontent.data);
+      res.json(generatedcontent.data);
+    } else {
+      console.log('error: content generation failed');
+    }
+  })();
+
 });
 
 module.exports = router;
